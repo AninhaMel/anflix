659480cf23612f17341b119d07b68e08012d8c81
AninhaMel <ana.silva5134@etec.sp.gov.br>
2023-05-16 08:39:24 -0300

    Mexemos no Movie e genre e criamoa a classe moviegenre

--- Anflix/Models/Genre.cs
+++ Anflix/Models/Genre.cs
@@ -1,12 +1,13 @@
-using System.ComponentModel.DataAnnotations; 
+using System.ComponentModel.DataAnnotations;
 
-namespace Anflix.Models;
+namespace ZazaFlix.Models;
 
-    public class Genre
-    {
-        [Key] //Define a propriedade como Chave Primária
-        public byte Id { get; set;}
-        [Required] // Requerido - Not Null, Validação
-        [StringLeght(30)] // Tamanho máximo da propriedade
-        public string Name { get; set; }
-    }
+public class Genre
+{
+    [Key] //Define a propriedade como Chave Primária. 
+    public byte Id {get; set; }
+
+    [Required] //Requerido - Not Null; Validação.
+    [StringLength(30)] // Tamanho máximo da propriedade.
+    public string Name { get; set; }
+}
